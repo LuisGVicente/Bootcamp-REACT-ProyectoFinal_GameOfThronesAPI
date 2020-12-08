3d15a9190465bc8a14535498e984c5054a94f3ee
dariocousin <darioblascoalcolado@gmail.com>
2020-12-08 16:17:20 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.scss';
+
+import { BrowserRouter as Router} from 'react-router-dom'
+import DetailsPage from './shared/DetailsPage/DetailsPage';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <DetailsPage></DetailsPage>
       </header>
     </div>
+    </Router>
   );
 }
 
--- src/shared/detailsPage/DetailsPage.jsx
+++ src/shared/detailsPage/DetailsPage.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import PortadaPage from '../PortadaPage/PortadaPage';
 
 export default function DetailsPage (){
 
-    
+    useEffect(() => {
+        axios.get('https://api.got.show/api/show/characters/').then(res => {
+            const house = res.data[0]
+        })
+    }, []);
+
     return (
 
         <div>
-
+            <PortadaPage values={house}></PortadaPage>
         </div>
     )
 }
