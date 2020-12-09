8296bbb898b72497f811ba06da3d5a012f14421e
dariocousin <darioblascoalcolado@gmail.com>
2020-12-09 12:15:32 +0100

    simplebar

--- src/shared/PortadaPage/PortadaPage.jsx
+++ src/shared/PortadaPage/PortadaPage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
 
-export default function PortadaPage(){
+export default function PortadaPage(props){
     return (
         <div>
-            
+            <figure>
+                <img src={props.values.image} alt={props.values.name} />
+                <figcaption>{props.values.name}</figcaption>
+            </figure>            
         </div>
     )
 }
--- src/shared/detailsPage/DetailsPage.jsx
+++ src/shared/detailsPage/DetailsPage.jsx
@@ -1,19 +1,53 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import PortadaPage from '../PortadaPage/PortadaPage';
 
+import SimpleBar from 'simplebar-react';
+import 'simplebar/dist/simplebar.min.css';
+
 export default function DetailsPage (){
+    
+    const[house, setDataHouse] = useState([]);
 
     useEffect(() => {
         axios.get('https://api.got.show/api/show/characters/').then(res => {
-            const house = res.data[0]
+            setDataHouse(res.data[5])
         })
     }, []);
 
+    // console.log(house.house);
     return (
 
-        <div>
+        <div className="">
             <PortadaPage values={house}></PortadaPage>
+            <div className="row">
+                <div className="col-12 col-sm-6 col-lg-2">
+                    <h3>Casa</h3>
+                    <img src='https://vignette.wikia.nocookie.net/gameofthrones/images/8/8a/House-Stark-Main-Shield.PNG/revision/latest/scale-to-width-down/350?cb=20170101103142' alt={house.house} />
+                </div>
+                <div className="col-12 col-sm-6 col-lg-2">
+                    <h3>Alianzas</h3>
+                    <p>{house.allegiances}</p>
+                </div>
+                <div className="col-12 col-sm-6 col-lg-2">
+                    <h3>Apariciones</h3>
+                    <SimpleBar style={{ maxHeight: 200 }}>
+                        <p>{house.appearances}</p>
+                    </SimpleBar>
+                </div>
+                <div className="col-12 col-sm-6 col-lg-2">
+                    <h3>Padre</h3>
+                    <p>{house.father}</p>
+                </div>
+                <div className="col-12 col-sm-6 col-lg-2">
+                    <h3>Descendientes</h3>
+                    <p>{house.siblings}</p>
+                </div>
+                <div className="col-12 col-sm-6 col-lg-2">
+                    <h3>Titulos</h3>
+                    <p>{house.titles}</p>
+                </div>
+            </div>
         </div>
     )
 }
