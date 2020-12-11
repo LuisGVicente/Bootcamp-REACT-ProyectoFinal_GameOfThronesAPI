2eb90ac95024ef554e6537b322b0ad233cff4af1
LuisGVicente_UpgradeHub <garcia.vicente@bootcamp-upgrade.com>
2020-12-11 15:54:21 +0100

    hover incluido

--- src/shared/Gallery/Gallery.jsx
+++ src/shared/Gallery/Gallery.jsx
@@ -12,13 +12,13 @@ export default function Gallery(props) {
     return (
         <div className="c-gallery ">
             <SimpleBar style={{ maxHeight: 500 }}>
-            <div className="row">
+            <div className="row container">
                 {props.items && props.items.map((item,index) => 
-                <div className="col-12 col-sm-4 col-lg-2" key={index}>
-                    <figure className="c-gallery__figure">
+                <div className="col-12 col-sm-4 col-lg-2 " key={index}>
+                    <figure className="c-gallery__figure image">
                         {item.image? <Link to={"characters/"+item.name}><img src={item.image} alt={item.name}/></Link> :
                         item.logoURL ? <Link to={"/houses/"+item.name}><img src={item.logoURL} alt={item.name}/></Link> : <Link to={"/houses/"+item.name}><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStx4gMG-ooM9ta3v_6RJV-U30GEQsQSV4TUw&usqp=CAU" alt=""/></Link>}
-                        {props.items? <figcaption>{item.name} from {item.house}</figcaption> : <figcaption>{item.name}</figcaption>}
+                        {props.items? <figcaption className="overlay">{item.name} from {item.house}</figcaption> : <figcaption>{item.name}</figcaption>}
                     </figure>
                 </div>
                     )}
