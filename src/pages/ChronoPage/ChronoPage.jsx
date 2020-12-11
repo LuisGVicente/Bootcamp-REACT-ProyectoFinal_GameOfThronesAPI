41f0e295d39785195b18e7b9bb8764429223184d
LuisGVicente_UpgradeHub <garcia.vicente@bootcamp-upgrade.com>
2020-12-11 14:10:26 +0100

    ChronoPage funcional

--- src/pages/ChronoPage/ChronoPage.jsx
+++ src/pages/ChronoPage/ChronoPage.jsx
@@ -11,24 +11,48 @@ import 'simplebar/dist/simplebar.min.css';
 export default function ChronoPage(props) {
     
     const [characters, setCharacters] = useState([]);
+    const [order, setOrder] = useState(true);
+
 
     useEffect(() => {
-
-        axios.get('https://api.got.show/api/show/characters/').then(res => {
-            setCharacters(res.data);
-            console.log(res.data);
-       
+        axios.get('https://api.got.show/api/show/characters/').then(res => {     
+        const ages = res.data.filter((cha) => cha.age && cha.age.age);
+        setCharacters(ages);
         })
     },[]);
+
+
+    function sortJSON(data, key, orden) {
+            return data.sort(function (a, b) {
+                var x = a.age[key],
+                y = b.age[key];
+
+                if (orden === 'asc') {
+                    return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+                }
+
+                if (orden === 'desc') {
+                    return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+                }
+            });
+    }
+
+    if (order === true) {
+        sortJSON(characters, 'age', 'desc');
+    } else {
+        sortJSON(characters, 'age', 'asc');
+    }
+
 
     return (
         <>
             <Traductor></Traductor>
+            <button onClick={() => setOrder(!order)}>Ordenar</button>
             <div className="container ">
         <SimpleBar style={{ maxHeight: 400 }}>
-        <div className="row">
+        <div >
             {characters.map((item,index) => 
-            <div className="col-lg-2" key={index}>
+            <div className="col-lg-4" key={index}>
             {item.age ? <p>{item.age.age}</p> : <p>?</p>}
             <figcaption>{item.name}</figcaption>
                 <figure  >
